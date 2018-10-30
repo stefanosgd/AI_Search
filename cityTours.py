895eacdd520a55fe4458747e3aae9f6697faade2
Stefanos Gregory Demetriou <sgdemetriou@gmail.com>
2018-10-30 15:11:07 +0000

    Add files via upload

--- cityTours.py
+++ cityTours.py
@@ -14,10 +14,6 @@ def readFile(fName):
     intData = intData[1:]
     return(intData, size)
 
-def printArray(a):
-    for x in a:
-        print(x)
-
 def toArray(size, cityData):
     cities = createEmpty(size)
     print(cityData)
@@ -30,10 +26,41 @@ def toArray(size, cityData):
             cities[j][i] = cityData[count]
             count += 1
     print(np.matrix(cities))
+    return cities
+
+def createTourGreedy(size, cityDistance):
+    tour = createEmptyTour(size)
+    currentCity=0
+    shortest = 100000
+    nextCity = 0
+    visited = 0
+    tourLength = 0
+    while (visited<size-1):
+        print(currentCity+1 , ",")
+        for i in range(0,size):
+           # print(cityDistance[currentCity][i])
+            if ((cityDistance[currentCity][i] < shortest) & (currentCity != i)):
+                if (tour[i] == 0):
+                    shortest = cityDistance[currentCity][i]
+                    nextCity = i
+        tour[currentCity] = nextCity
+        currentCity = nextCity
+        tourLength += shortest
+        shortest = 100000
+        visited += 1
+        #print(visited)
+    tourLength += cityDistance[currentCity][0]
+    print(currentCity+1)
+    print()
+    print(tourLength)
+
+def createEmptyTour(s):
+    return [0 for x in range(0,s)]
 
 def createEmpty(s):
     return [[0 for x in range(0,s)]for y in range(0,s)]
 
 def run():    
-    data, size = readFile('AISearchtestcase.txt')
-    toArray(size, data)
+    data, size = readFile('NEWAISearchfile026.txt')
+    cities = toArray(size, data)
+    createTourGreedy(size, cities)
